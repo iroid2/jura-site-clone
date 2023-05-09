c2291792b03c4f9a9eaf65a7d58eeb48433bd992
TUKAHEEBWARUHANGA IRAD <104001652+iroid2@users.noreply.github.com>
2023-05-09 12:57:02 +0300

    .

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+const works = new Swiper('#works', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 100,
+        disableOnInteraction: false,
+      },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
