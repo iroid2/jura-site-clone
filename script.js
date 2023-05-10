bd6e980d3c249a72d7dc19605977743cea4fc057
TUKAHEEBWARUHANGA IRAD <104001652+iroid2@users.noreply.github.com>
2023-05-10 12:43:49 +0300

    .

--- script.js
+++ script.js
@@ -24,31 +24,23 @@ const swiper = new Swiper('.swiper', {
   });
   var works = new Swiper("#works", {
     slidesPerView: 3,
-    spaceBetween: 5,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween:20,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    breakpoints: {
-      120: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
-      },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
     },
   });
